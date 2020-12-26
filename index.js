e94aeeefc25392b2d592a14385b0a48197a9d088
Jana Zrayka <janjoune.97@hotmail.com>
2020-12-26 17:17:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,23 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
+/* Write your implementationof greet() */
+let hours= ' '+ ':'+ ' ';
+let nb = Number(hours);
+const greets = ["Good Morning","Good Afternoon","Good Evening"];
+function greet(hours){
+  if(hours <= '12:00'){
+  return greets[0];}
+  else 
+  if(hours>'12:00' && hours<='17:00'){
+  return greets[1];}
+  else {
+  return greets[2];}
+}
 /* Write your implementation of displayMessage() */
+ function displayMessage(message){
+   let msg= 'TEST';
+   document.getElementById('greeting').innerText =msg;
+   
+   
+ }
